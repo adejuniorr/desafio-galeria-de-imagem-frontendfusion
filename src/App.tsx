654974b9b0f882469660a52b7365ecbcfacbbf55
Ademir Júnior <adejr0220@gmail.com>
2024-10-13 20:31:59 -0300

    :wrench: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import { ImageList } from "./components/ImageList";
-import { ImagesFilter } from "./components/ImagesFilter";
 
 function App() {
   return (
-<<<<<<< Updated upstream
-    <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Desafio de Galeria de Imagens - Processo Seletivo Frontend Fusion</p>
-      </main>
-    </>
-=======
     <main className="flex flex-col items-center">
       <ImageList />
     </main>
->>>>>>> Stashed changes
   );
 }
 
