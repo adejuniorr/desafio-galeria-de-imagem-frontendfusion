07d9c211351b88d604b02ccd446a8b258b983baa
Ademir Júnior <adejr0220@gmail.com>
2024-10-17 18:01:58 -0300

    :arrow-up: update ImageList
    Add Modal component to view details of images

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,11 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { ImagesContext } from "../contexts/ImagesContext";
-import { ImagesFilter } from "./ImagesFilter";
 import { FaPlus } from "react-icons/fa";
 import { Modal } from "../components/Modal";
 
 export const ImageList = () => {
-  const { images, currentImageList, updateCurrentImages } = useContext(ImagesContext);
+  const { images, currentImageList, updateCurrentImages } =
+    useContext(ImagesContext);
   const [imageDetailsOpen, setImageDetailsOpen] = useState<boolean>(false);
   const [modalInfo, setModalInfo] = useState<{}>({});
 
@@ -14,19 +14,18 @@ export const ImageList = () => {
   }, [images]);
 
   const handleOpenModal = (imageInfo: {}) => {
-    setImageDetailsOpen(true);
+    setImageDetailsOpen(!imageDetailsOpen);
     setModalInfo(imageInfo);
   };
 
   return (
     <>
-      <ImagesFilter images={images} setImageList={updateCurrentImages} />
-      <div className="pb-24 columns-3 gap-0 sm:gap-[8px] mt-8 ">
+      <div className="pb-24 columns-2 sm:columns-3 gap-0 sm:gap-[8px] px-3 lg:px-[8vw]">
         {currentImageList
           .sort((a: any, b: any) => b.height - a.height)
           .map((image: any) => (
             <div className="relative h-fit sm:mb-[8px]" key={image.id}>
-              <div className="relative overflow-hidden max-w-[35vw] border border-gray-200">
+              <div className="relative overflow-hidden max-w-[46vw] sm:max-w-[35vw] border border-gray-200">
                 <div
                   className="sm:hidden w-[100%] h-[100%] bg-transparent absolute"
                   onClick={() => handleOpenModal(image)}
@@ -49,6 +48,6 @@ export const ImageList = () => {
         setImageDetailsOpen={setImageDetailsOpen}
         modalInfo={modalInfo}
       />
-    </>
+      </>
   );
 };
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -28,15 +28,15 @@ export const Modal = ({
       id="default-modal"
       aria-hidden="true"
       className={clsx(
-        "w-full h-full bg-black bg-opacity-30 overflow-y-auto overflow-x-hidden z-50 flex justify-center items-center",
+        "w-full h-full bg-black bg-opacity-30 overflow-y-auto overflow-x-hidden fixed top-0 left-0 z-50 flex justify-center items-center",
         {
-          fixed: imageDetailsOpen,
-          hidden: !imageDetailsOpen,
+          fixed: imageDetailsOpen === true,
+          hidden: imageDetailsOpen === false,
         }
       )}
       onClick={(e) => {
         e.stopPropagation();
-        setImageDetailsOpen(false);
+        setImageDetailsOpen(!imageDetailsOpen);
       }}
     >
       <div
@@ -45,29 +45,29 @@ export const Modal = ({
         }}
         className="h-fit overflow-y-hidden bg-white border border-gray-200 rounded-lg shadow"
       >
-        <div className="flex items-center justify-between p-4 border-b ">
-          <h3 className="text-xl font-semibold text-gray-900 ">Detalhes</h3>
+        <div className="flex items-center justify-between p-4 border-b text-[1.2rem] sm:text-[1.6rem]">
+          <h3 className="font-semibold text-gray-900 ">Detalhes</h3>
           <button
-            onClick={() => setImageDetailsOpen(false)}
+            onClick={() => setImageDetailsOpen(!imageDetailsOpen)}
             type="button"
-            className="text-gray-800 bg-transparent hover:bg-gray-200 text-sm w-8 h-8 ms-auto flex justify-center items-center rounded-md"
+            className="text-gray-800 bg-transparent hover:bg-gray-200 w-8 h-8 ms-auto flex justify-center items-center rounded-md"
             data-modal-hide="default-modal"
           >
             <FaXmark />
           </button>
         </div>
-        <div className="flex flex-col w-fit">
-          <div className="w-[20vw]">
+        <div className="flex flex-col w-[90vw] sm:w-[50vw]">
+          <div className="w-[100%] max-h-[40vh] overflow-hidden flex items-center">
             <img
               className="object-contain"
               src={modalInfo.download_url}
               alt=""
             />
           </div>
-          <div className="relative p-4">
+          <div className="relative p-4 sm:text-[1.3rem]">
             <div>
               <p className="text-gray-400">#{modalInfo.id}</p>
-              <h2 className="font-bold text-xl">
+              <h2 className="font-bold  sm:text-[1.8rem]">
                 Autor(a): {modalInfo.author}
               </h2>
               <p>
@@ -82,7 +82,7 @@ export const Modal = ({
               >
                 <FaRegHeart className="text-red-500" />
               </button>
-              <div className="flex justify-end items-center gap-4 mt-6">
+              <div className="flex justify-end items-center gap-4 md:text-[1.2rem]">
                 <a
                   target="_blank"
                   href={modalInfo.url}
@@ -96,7 +96,7 @@ export const Modal = ({
                   target="_blank"
                   href={modalInfo.download_url}
                   onClick={() => download()}
-                  className="flex items-center gap-[4px] cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-lg text-sm px-5 py-2 text-center"
+                  className="flex items-center gap-[4px] cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-600 rounded-lg px-5 py-2 text-center"
                 >
                   <FaDownload />
                   Baixar
